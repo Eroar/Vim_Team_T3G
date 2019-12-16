b2935e8d12a1b5a460a5bb6edc7c49c399e78220
SupahHackah1 <56177111+SupahHackah1@users.noreply.github.com>
2019-12-16 14:23:50 +0100

    penis

---
+++ VimStuff/Assets/BallFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallFactory : MonoBehaviour
+{
+    [SerializeField] GameObject pball;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    GameObject createBall(Vector2 pos)
+    {
+        GameObject g;
+        g = Instantiate(pball);
+        g.transform.localPosition = pos;
+        g.transform.SetParent(this.transform);
+        return g;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
